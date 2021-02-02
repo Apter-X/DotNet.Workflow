59e189f09b4db3c9ded32e185d8d95da0c66d675
Iliass Raihani <i.raihani@outlook.com>
2021-02-02 15:39:28 +0100

    init dotnet core console project

---
+++ DotNet.Core.Console.Exemple/DotNet.Core.Console.Exemple/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet.Core.Console.Exemple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
